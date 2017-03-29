1839ed21bb65d5f104fb5f9f1ee8159564dbdfd2
mywaterfly74 <Leoleo123>
2017-03-29 20:07:03 +0500

    1

---
+++ Pyatna/IPlayable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Pyatna
+{
+    public interface IPlayable
+    {
+        void Randomize();
+        bool IsFinished();
+        void Shift(int value);
+        int GetLength();
+        int this[int x, int y]
+        {
+            get;
+        }
+    }
+}
---
+++ Pyatna/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Pyatna
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] xx = new int[1024];
+            for (int i = 0; i < 1024; ++i)
+                xx[i] = i;
+            IPlayable game = new Game2(xx);
+            ConsoleGameUI CG = new ConsoleGameUI(game);
+            CG.StartGame();
+        }
+    }
+}
